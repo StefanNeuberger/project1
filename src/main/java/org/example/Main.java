4bea61ecbaef3a4d7c0cd4444741b5c979b1722a
StefanNeuberger <stef.neuberger@gmail.com>
2025-10-15 10:28:29 +0200

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            System.out.println("i = " + i);
+        }
+    }
+}
