b8ef8b6f80fdefa37f3ba02afb9b0a4170acf4a1
StefanNeuberger <stef.neuberger@gmail.com>
2025-10-15 11:05:12 +0200

    add basic methods

--- src/main/java/org/example/PasswordValidator.java
+++ src/main/java/org/example/PasswordValidator.java
@@ -9,4 +9,33 @@ package org.example;
 public final class PasswordValidator implements PasswordValidatorSpec {
 
 
+    @Override
+    public boolean hasMinLength(String password, int min) {
+        return false;
+    }
+
+    @Override
+    public boolean containsDigit(String password) {
+        return false;
+    }
+
+    @Override
+    public boolean containsUpperAndLower(String password) {
+        return false;
+    }
+
+    @Override
+    public boolean isCommonPassword(String password) {
+        return false;
+    }
+
+    @Override
+    public boolean containsSpecialChar(String password, String allowed) {
+        return false;
+    }
+
+    @Override
+    public boolean isValid(String password) {
+        return false;
+    }
 }
